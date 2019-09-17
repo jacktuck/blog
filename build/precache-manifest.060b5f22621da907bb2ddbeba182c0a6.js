19ad73c9381c9f2199fbf2f78ec6351d80868389
Jack Tuck <jack-tuck@hotmail.com>
2019-09-17 10:53:07 +0100

    build

---
+++ build/precache-manifest.060b5f22621da907bb2ddbeba182c0a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea96535c2dc81e0565dd58fa34ce837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a4ff6b4f9860fabbce",
+    "url": "/static/css/main.1a3f837c.chunk.css"
+  },
+  {
+    "revision": "2f2be65b1dc1939c18d8",
+    "url": "/static/js/2.cb5bae83.chunk.js"
+  },
+  {
+    "revision": "63a4ff6b4f9860fabbce",
+    "url": "/static/js/main.76d34fd2.chunk.js"
+  },
+  {
+    "revision": "1df1c628547308f4dbcc",
+    "url": "/static/js/runtime~main.3ec8825d.js"
+  }
+]);
